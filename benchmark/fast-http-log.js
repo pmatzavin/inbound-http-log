162362d76df86798acc7907375093dfb70a51ae5
matzavinos <matzavinos@workable.com>
2018-02-20 11:29:35 +0200

    bench: Add "request.id" in the benchmark script.

--- benchmark/fast-http-log.js
+++ benchmark/fast-http-log.js
@@ -1,4 +1,21 @@
-require('../index')();
+// I copied this fn from the http-pino source project
+// Just to be fair in the benchmark comparison
+// Becaue by default:
+//// the fast-http-log module will just execute a dull function:
+//// () => null
+//// and does not log a request id
+function reqIdGenFactory (func) {
+    if (typeof func === 'function') return func
+    var maxInt = 2147483647
+    var nextReqId = 0
+    return function genReqId (req) {
+      return req.id || (nextReqId = (nextReqId + 1) & maxInt)
+    }
+}
+
+require('../index')({
+    getRequestId: reqIdGenFactory()
+});
 
 const server = require('./server');
 const port = 3001;
